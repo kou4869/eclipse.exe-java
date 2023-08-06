56c6c642c7d3ffa9a3e0a924732f0a5bbeaac1bf
Sample123 <119918517+kou4869@users.noreply.github.com>
2023-08-06 17:30:03 +0900

    tuika

---
+++ src/HalloWorldClass.java
@@ -0,0 +1,11 @@
+
+public class HalloWorldClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo World");
+		System.out.println("こんにちは");
+		System.out.println("世界");
+
+	}
+
+}
